2ca170edc53b48571d9fdbe639dddfd2af144aff
Miguel Muñoz Caro <miguelmc.dev@gmail.com>
2024-07-04 17:50:45 -0400

    event: se agrega imageUrl y ticketDetails como array

--- src/models/event.model.js
+++ src/models/event.model.js
@@ -6,8 +6,9 @@ const eventSchema = new mongoose.Schema({
     name: { type: String, required: true },
     date: { type: Date, required: true},
     location: { type: String, required: true },
+    imageUrl: { type: String, required: true },
     capacity: { type: Number, required: true },
-    ticketDetails: { type: mongoose.Schema.Types.ObjectId, ref: 'tickets', required: true },
+    ticketDetails: [{ type: mongoose.Schema.Types.ObjectId, ref: 'tickets', required: true }],
 }, {
     timestamps: true
 });
